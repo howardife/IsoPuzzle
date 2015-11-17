aa41e0aac2dbdd3ec2605a055de82f2bdb821978
howardife <ifehoward@gmail.com>
2015-11-17 21:33:55 +0000

    Managers Added
    Prefab Factory
    Level Manager
    Audio Manager
    GameManager

---
+++ IsoPuzzleGame_Unity/Assets/Scripts/Managers/PrefabFactory.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class PrefabFactory {
+
+
+    //Statics====================================================================================
+    public static GameObject Player_Prefab;
+
+
+
+    //-------------------------------------------------------------------------------------------
+
+
+    //Static Methods===============================================================================
+    public static void InitPrefabs()
+    {
+        //assign prefabs with Resources.Load("") as GameObject
+    }
+
+    //-----------------------------------------------------------------------------------------------
+}
